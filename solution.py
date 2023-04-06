a6c8a41e550fb49c589644a4bb7bff79698ac33e
NikitaNikitchenko <62266898+NikitaNikitchenko@users.noreply.github.com>
2023-04-06 21:49:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 784664358 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    
+    
+    mean = x.mean()
+    var = x.var()
+    return np.log(((mean-743)**2 - var/len(x))/(mean-743)) # Ваш ответ
